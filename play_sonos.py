5543ac1ed55f4bf30c38296b42917b011fb5591c
Marco <viaimperiale@gmail.com>
2021-07-30 14:57:25 +0200

    shuffle then play next

--- play_sonos.py
+++ play_sonos.py
@@ -3,6 +3,9 @@ from logger import logging
 from controller import coordinator, pretty_dict
 
 try:
+    coordinator.shuffle = False
+    coordinator.shuffle = True
+    coordinator.next()
     coordinator.play()
     logging.info(pretty_dict(coordinator.get_current_track_info()))
 except Exception as e:
